8a4e1c9a079d1afb110d4af0873a184afb3773b2
Jonathan Freed <jon.freed@gmail.com>
2025-11-28 15:37:19 +0700

    Add support page, update styles, and fix i18n bug

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
+        support: 'support.html',
         privacy: 'privacy.html',
         terms: 'terms.html',
         'privacy-ja': 'privacy-ja.html',
